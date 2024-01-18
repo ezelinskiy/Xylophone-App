2f31798fa83d3877bd9aca1fdf061adc03432fa5
Yevhen Zelinskyi <3elinskiy@gmail.com>
2024-01-18 11:02:07 +0200

    Added all functionality

--- Xylophone App/ViewController.swift
+++ Xylophone App/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import AVFoundation
+
+class XylophoneButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layer.cornerRadius = 15
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.layer.cornerRadius = 15
+    }
+}
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var player: AVAudioPlayer!
+    
+    @IBAction func onKeyPressed(_ sender: UIButton) {
+        playSoundWith(name: sender.currentTitle ?? "A")
+        sender.alpha = 0.7
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
     }
-
-
+    
+    func playSoundWith(name: String) {
+        let url = Bundle.main.url(forResource: name, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
 
